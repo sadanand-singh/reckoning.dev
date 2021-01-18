1e667e6a513f95e5d35c69ee30ecd6bdf8b3181f
Sadanand Singh <sadanand4singh@gmail.com>
2021-01-18 14:21:31 -0800

    update data files for site authors etc

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,9 +1,9 @@
 module.exports = {
-  title: 'Vredeburg',
-  description: 'Eleventy starter project to create a blog',
+  title: 'reckoning.dev',
+  description: "Sadanand's blog and notes",
   keywords: ['eleventy', 'template', 'simple', 'clean'],
   // your site url without trailing slash
-  url: 'https://vredeburg.netlify.app',
+  url: 'https://reckoning.dev',
   // how many posts you want to show for each page
   paginate: 6
   // if you want to add disqus to your site
